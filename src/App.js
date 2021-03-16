b93f95b755b40bce0f9bfafdeb5062d08312dbe5
Ryan Odd <ryanodd@gmail.com>
2021-03-16 11:01:54 -0400

    Added id to test FormattedMessage

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <FormattedMessage
+          id="stringId"
           default="hellooooooo world!"
         />
         <a
